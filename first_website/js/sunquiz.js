ce611630527117ec356ab073fce14705362027bf
amyjohnstone <amyjohnstone98@googlemail.com>
2018-11-21 18:56:48 +0000

    quiz stuff - does not work

--- first_website/js/sunquiz.js
+++ first_website/js/sunquiz.js
@@ -1,3 +1,23 @@
 function check() {
-document.getElementById("aftwe_submit").style.visibility= "visible";
+
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var question4 = document.quiz.question4.value;
+var correct = 0;
+
+if (question1 == "True" ) {
+    correct++;
+}
+if (question2 == "True") {
+  correct++;
+}
+if (question3 == "False") {
+  correct++;
+}
+  if (question4 == "True") {
+    correct++;
+}
+document.getElementById("aftersubmit").style.visibility= "visible";
+document.getElementById("numbercorrect").innerHTML = "Well done! You got " + correct + "correct";
 }
